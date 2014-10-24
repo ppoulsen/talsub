ffb89011326db9d4eb5353b27690a6879fc45c53
ppoulsen <ppoulsen21@gmail.com>
2014-10-23 22:37:59 -0500

    Added API call to query TAL episodes by language.

--- TalSub/web/talsub/views.py
+++ TalSub/web/talsub/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed
+import json
+
+from data.dao import EpisodeDAO
 
 def home(request):
     return render(request, 'index.html')
+
+
+def episode_list(request):
+    """
+    Gets all the episodes with correct language code from GET parameter
+    :param request: HttpRequest bearing language code in GET
+    :return: JSON of all TAL titles, audio links, and episode number
+    """
+    if request.method != 'GET':
+        return HttpResponseNotAllowed(request)
+
+    langCode = request.GET['lang']
+
+    dao = EpisodeDAO()
+    dtos = dao.find('number', 'title', 'audio', languages=langCode).order_by('number')
+    clean_vals = []
+    for dto in dtos:
+        clean_vals.append({
+            'number': dto.number,
+            'title': dto.title,
+            'audio': dto.audio,
+        })
+
+    ret = json.dumps(clean_vals)
+
+    return HttpResponse(ret, content_type='application/json')
--- TalSub/web/web/urls.py
+++ TalSub/web/web/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'talsub.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^episode_list/$', 'talsub.views.episode_list', name='episode_list'),
 
     #url(r'^admin/', include(admin.site.urls)),
 )
